1532f59ec92664d8bee34fc1e116df6509789e6d
Megakochan <133385626+Megakochan@users.noreply.github.com>
2026-07-07 02:37:13 -0400

    Add files via upload

---
+++ Top Nav.js
@@ -0,0 +1,19 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("myTopnav").style.top = "0";
+  } else {
+    document.getElementById("myTopnav").style.top = "-75px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
